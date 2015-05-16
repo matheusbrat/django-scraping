2103574cc06f57bf8e573d9f70fff1aee1b642cf
Matheus Bratfisch <matheusbrat@gmail.com>
2015-05-16 15:10:03 -0300

    Fixing production settings file

--- djangoscraping/settings/production.py
+++ djangoscraping/settings/production.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'djangoscraping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+DATABASES = {}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
