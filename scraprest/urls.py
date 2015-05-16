b56f615545ccccef2d30019bcc89a6c0f97573e2
Matheus Bratfisch <matheusbrat@gmail.com>
2015-05-16 15:21:15 -0300

    Changing endpoint route just to test automatic deploy

--- scraprest/urls.py
+++ scraprest/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from scraprest.views.views import ArticleViewSet, OutletViewSet, WriterViewSet
 
 router = routers.DefaultRouter()
-router.register(r'articles', ArticleViewSet)
+router.register(r'articles_test', ArticleViewSet)
 router.register(r'outlets', OutletViewSet)
 router.register(r'writers', WriterViewSet)
