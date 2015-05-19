312d1b7f296a2d3cc99d4d735b0954f09c46d2b3
Matheus Bratfisch <matheusbrat@gmail.com>
2015-05-18 21:36:27 -0300

    Adding new field to Article model, specifying some unique fields on model

--- scrapingapp/models.py
+++ scrapingapp/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 class Outlet(models.Model):
 	name = models.CharField(max_length=255)
-	url  = models.CharField(max_length=255)
+	url  = models.CharField(max_length=255, unique=True)
 	description = models.CharField(max_length=255)
 
 class Writer(models.Model):
 	name = models.CharField(max_length=255)
 	twitter = models.CharField(max_length=255)
-	profile = models.CharField(max_length=255)
+	profile = models.CharField(max_length=255, unique=True)
 
 class Article(models.Model):
 	writers = models.ManyToManyField(Writer)
 	outlet = models.ForeignKey(Outlet)
 	publication_date = models.DateField()
 	content = models.TextField()
+	url = models.URLField(null=True)
